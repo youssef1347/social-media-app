5dc42130590a3dce20918f9aea850b18fa8fce9b
youssef1347 <husseinyoussef589@gmail.com>
2026-01-22 15:25:47 -0800

    ..

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './store/store.jsx'
+import store from './store/store.js'
 import './index.css'
 import App from './App.jsx'
 
