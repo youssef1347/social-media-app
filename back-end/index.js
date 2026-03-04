9193fbf966f4aeab33c03238aa49444da62e7f72
youssef1347 <husseinyoussef589@gmail.com>
2026-03-04 14:02:07 -0800

    ...

--- back-end/index.js
+++ back-end/index.js
@@ -16,6 +16,7 @@ const limiter = rateLimit({ windowMs: 15 * 60 * 1000, limit: 100 });
 
 const app = express();
 app.use(express.json());
+app.use(express.static());
 app.use(cookieParser());
 app.use(cors({ origin: '*', credentials: true }));
 app.use(limiter);
